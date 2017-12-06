3bcc032da550a15d1263bf8fac64018156d4565c
Anderson <Anderson@192.168.25.4>
2017-12-06 00:22:50 -0200

    Inclusão da configuração do Jpa

--- psicologia-web/src/main/java/br/com/jope/psicologia/configuration/WebInitializer.java
+++ psicologia-web/src/main/java/br/com/jope/psicologia/configuration/WebInitializer.java
@@ -10,7 +10,7 @@ public class WebInitializer extends AbstractAnnotationConfigDispatcherServletIni
  
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return new Class[] { WebConfig.class };
+        return new Class[] { WebConfig.class, PersistenceJPAConfig.class };
     }
   
     @Override
